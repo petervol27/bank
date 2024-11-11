4c71a5f15d32c487dd51ffb98b3b5ea5762c12e8
Pietro <petervol35@gmail.com>
2024-11-11 22:03:11 +0200

    fixed

--- transactions/models.py
+++ transactions/models.py
@@ -31,9 +31,7 @@ class Transaction(models.Model):
         null=True,
         blank=True,
     )
-    transaction_type = models.CharField(
-        max_length=20, choices=TRANSACTION_TYPES
-    ).capitalize()
+    transaction_type = models.CharField(max_length=20, choices=TRANSACTION_TYPES)
     sender_new_balance = models.DecimalField(
         max_digits=10, decimal_places=2, null=True, blank=True
     )
