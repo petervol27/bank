1cb2975b07f3d15d4b71bab9bf718ce0ec33688f
Pietro <petervol35@gmail.com>
2024-11-12 20:25:45 +0200

    static files

--- bank/settings.py
+++ bank/settings.py
@@ -151,7 +151,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "staticfiles/"
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
     STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
