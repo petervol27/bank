301326d8dbca5457c940e14f6a5af9397d854805
peter vol <petervol35@gmail.com>
2025-01-10 15:49:39 +0200

    changed token

--- bank/settings.py
+++ bank/settings.py
@@ -173,7 +173,6 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=365),
-    # "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(minutes=20),
 }
