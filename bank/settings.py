5707fa10ad19d8f7b870ddfd249c3729fa4fd793
peter vol <petervol35@gmail.com>
2025-01-10 16:32:55 +0200

    fixed cors again

--- bank/settings.py
+++ bank/settings.py
@@ -41,7 +41,7 @@ CORS_ALLOWED_ORIGINS = [
     "https://bank-front-xwed.onrender.com",
     "https://bank-zdpd.onrender.com",
     "http://localhost:3000",
-    "https://bank-react-jq1e.onrender.com/",
+    "https://bank-react-jq1e.onrender.com",
 ]
 
 
