350de36c6d64df3cd4d54459760dc197e098baed
peter vol <petervol35@gmail.com>
2025-01-14 00:06:10 +0200

    fixed token lifetime

--- bank/settings.py
+++ bank/settings.py
@@ -174,8 +174,6 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    # "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
-    # "REFRESH_TOKEN_LIFETIME": timedelta(minutes=20),
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
-    "REFRESH_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(minutes=20),
 }
